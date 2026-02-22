213296d842c4850ec0431dea661bd4b6b6a85ad4
Amine BEN ZAGGAGH <amine.benzaggagh@doctorspolis.com>
2026-02-22 19:19:48 +0100

    Functional Programming - Supplier

---
+++ src/com/java/functionalInterfaces/SupplierExemple.java
@@ -0,0 +1,26 @@
+package src.com.java.functionalInterfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+import src.com.java.data.Student;
+import src.com.java.data.StudentDatabase;
+
+public class SupplierExemple {
+    
+    public static void main(String[] args) {
+        Supplier<Student> supplier = () -> {
+            return new Student("Alice", 10, 3.5, "Female", Arrays.asList("Basketball", "Chess"));
+        };
+
+        Supplier<List<Student>> supplierList = () -> {
+            return StudentDatabase.getAllStudents();
+        };
+
+        System.out.println(supplier.get());
+
+        System.out.println(supplierList.get());
+    }
+
+}
